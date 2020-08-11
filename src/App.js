68dfde0cc9ae393364aec725a183459e2b1f8563
AsusOnePlus X <onex@AsusOnePlusX.local>
2020-08-11 18:01:41 -0500

    solving exercise

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    checked: false
+  }
+
+  handleChange = (event) =>{
+    this.setState({
+      checked: (event.target.checked)
+    })
+    
+  }
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <label><input onChange={this.handleChange} type="checkbox" /> Mostrar información importante</label>
+        {this.state.checked ? <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> : ''}
       </div>
     );
   }
